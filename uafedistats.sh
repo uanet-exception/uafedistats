e481df9e77fcc00856a2487ea2edf929d44b6594
ZEN <dmytro.poltavchenko@gmail.com>
2023-02-11 19:10:18 +0200

    Cache responses in case of network errors

--- uafedistats.sh
+++ uafedistats.sh
@@ -5,7 +5,7 @@ set -e;
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )";
 
 # Dependencies check
-for COMMAND in curl jq tac gnuplot; do
+for COMMAND in curl jq tac grep mv mktemp sed gnuplot; do
     which $COMMAND &> /dev/null || {
         echo "[$(date -u +"%d-%m-%Y %H:%M:%S")] [ERROR] Unresolved dependencies: $COMMAND" 1>&2;
         exit 1;
@@ -32,9 +32,41 @@ function usage() {
     echo -e "\tpost - draw and post graph" 1>&2;
 }
 
+function get_nodeinfo_cached() {
+    local NODEINFO_URL;
+    local NODEINFO;
+    NODEINFO_URL=$(
+        curl -s -f --retry-delay 2 --retry 5 --connect-timeout 8 -m 10 \
+        -H "Accept: application/json" "https://$SERVER/.well-known/nodeinfo" \
+            | jq -r .links[-1].href 2>/dev/null \
+        || true
+    );
+    test -n "$NODEINFO_URL" && NODEINFO=$(
+        curl -s -f --retry-delay 2 --retry 5 --connect-timeout 8 -m 10 \
+        -H "Accept: application/json" "$NODEINFO_URL" || true
+    );
+    test -n "$NODEINFO" && {
+        TMP="$(mktemp)";
+        grep -v "^$SERVER " "$DIR/workspace/cache.db" 2>/dev/null > "$TMP";
+        mv "$TMP" "$DIR/workspace/cache.db";
+        echo -n "$SERVER " >> "$DIR/workspace/cache.db";
+        echo "$NODEINFO" | jq -c >> "$DIR/workspace/cache.db";
+        echo "$NODEINFO";
+        return 0;
+    }
+
+    echo "[$(date -u +"%d-%m-%Y %H:%M:%S")] [WARNING] $SERVER is not reachable, checking for cached response..." 1>&2;
+    grep -m1 "^$SERVER " "$DIR/workspace/cache.db" 2>/dev/null | sed "s/^$SERVER //";
+}
+
 function update() {
     SERVERS=(
-        $(curl -s -f --retry 5 --connect-timeout 8 -m 10 https://relay.social.net.ua/nodeinfo/2.1.json | jq -r .metadata.peers[] 2>/dev/null; true)
+        $(
+            curl -s -f --retry 5 --connect-timeout 8 -m 10 \
+            -H "Accept: application/json" https://relay.social.net.ua/nodeinfo/2.1.json \
+                | jq -r .metadata.peers[] 2>/dev/null \
+            || true
+        )
     );
 
     test "${#SERVERS[@]}" -gt 0 || {
@@ -46,19 +78,14 @@ function update() {
     declare -i TOTAL_USERS=0;
     declare -i LOCAL_POSTS=0;
     for SERVER in ${SERVERS[@]}; do
-        NODEINFO_URL=$(curl -s -f --retry-delay 2 --retry 5 --connect-timeout 8 -m 10 "https://$SERVER/.well-known/nodeinfo" | jq -r .links[-1].href 2>/dev/null; true);
-        test -n "$NODEINFO_URL" || {
-            echo "[$(date -u +"%d-%m-%Y %H:%M:%S")] [WARNING] Can't get nodeinfo url from $SERVER" 1>&2;
-            continue;
-        };
-        NODEINFO=$(curl -s -f --retry-delay 2 --retry 5 --connect-timeout 8 -m 10 "$NODEINFO_URL"; true);
+        NODEINFO=$(get_nodeinfo_cached "$SERVER");
         test -n "$NODEINFO" || {
             echo "[$(date -u +"%d-%m-%Y %H:%M:%S")] [WARNING] Can't get nodeinfo from $SERVER" 1>&2;
             continue;
         };
         let SERVERS_ACTIVE+=1;
-        let TOTAL_USERS+=$(echo $NODEINFO | jq -r '.usage.users.total // 0');
-        let LOCAL_POSTS+=$(echo $NODEINFO | jq -r '.usage.localPosts // 0');
+        let TOTAL_USERS+=$(echo "$NODEINFO" | jq -r '.usage.users.total // 0');
+        let LOCAL_POSTS+=$(echo "$NODEINFO" | jq -r '.usage.localPosts // 0');
     done
 
     echo "$(date +%s),$TOTAL_USERS,$SERVERS_ACTIVE,$LOCAL_POSTS" >> "$DIR/workspace/mastostats.csv";
@@ -93,7 +120,7 @@ function post() {
         test "$DATE" -gt "$ONE_HOUR_AGO" && LAST_HOUR_USERS=$USERS;
         test "$DATE" -gt "$ONE_DAY_AGO" && test "$USERS" -lt "$LAST_HOUR_USERS" && LAST_DAY_USERS=$USERS;
         test "$DATE" -gt "$ONE_WEEK_AGO" && test "$USERS" -lt "$LAST_DAY_USERS" && LAST_WEEK_USERS=$USERS;
-        test "$USERS" -gt "$MAX_USERS" && MAX_USERS=$USERS;
+        test "$MAX_USERS" -eq 0 && MAX_USERS=$USERS;
     done < <(tac "$DIR/workspace/mastostats.csv");
 
     # Normalize stats if no recent data
@@ -105,6 +132,12 @@ function post() {
     echo "[$(date -u +"%d-%m-%Y %H:%M:%S")] [INFO] +$((MAX_USERS - LAST_HOUR_USERS)) in the last hour" 2>&1;
     echo "[$(date -u +"%d-%m-%Y %H:%M:%S")] [INFO] +$((MAX_USERS - LAST_DAY_USERS)) in the last day" 2>&1;
     echo "[$(date -u +"%d-%m-%Y %H:%M:%S")] [INFO] +$((MAX_USERS - LAST_WEEK_USERS)) in the last week" 2>&1;
+
+    test "$(($MAX_USERS - $LAST_HOUR_USERS))" -eq 0 && {
+        echo "[$(date -u +"%d-%m-%Y %H:%M:%S")] [INFO] No new users for the past hour, exiting..." 2>&1;
+        return 0;
+    }
+    exit;
 
     echo "[$(date -u +"%d-%m-%Y %H:%M:%S")] [INFO] executing generate.gnuplot:";
     cd "$DIR" && gnuplot "$DIR/generate.gnuplot" 1>&2;
